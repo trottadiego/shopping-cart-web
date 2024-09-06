0be8aea4dbc59a15857189b847a6a7d913655300
dtrotta <ditrotta@falabella.com.ar>
2024-09-06 10:32:52 -0300

    fix default route

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { Route, Routes } from "react-router-dom";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import Cart from "./pages/Cart";
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import Cart from './pages/Cart';
 
 function App() {
   return (
     <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/cart" element={<Cart />} />
+      <Route path='*' element={<Login />} />
+      <Route path='/dashboard' element={<Dashboard />} />
+      <Route path='/cart' element={<Cart />} />
     </Routes>
   );
 }
--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,19 +1,17 @@
-import React, { createContext, useContext, useCallback } from "react";
+import React, { createContext, useContext, useCallback } from 'react';
 import {
   fetchCartData,
   addToCartService,
   updateCartService,
   removeFromCartService,
-} from "../services/cartServices";
-import { CartContextProps, CartState, ProductInCart } from "../types/CartTypes";
-import { Product } from "../types/ProductTypes";
-import { useNavigate } from "react-router-dom";
+} from '../services/cartServices';
+import { CartContextProps, CartState, ProductInCart } from '../types/CartTypes';
+import { Product } from '../types/ProductTypes';
+import { useNavigate } from 'react-router-dom';
 
 const CartContext = createContext<CartContextProps | undefined>(undefined);
 
-export const CartProvider: React.FC<{ children: React.ReactNode }> = ({
-  children,
-}) => {
+export const CartProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [cartState, setCartState] = React.useState<CartState>({
     products: [],
   });
@@ -21,9 +19,9 @@ export const CartProvider: React.FC<{ children: React.ReactNode }> = ({
   const navigate = useNavigate();
 
   const getCart = useCallback(async () => {
-    const token = localStorage.getItem("token");
+    const token = localStorage.getItem('token');
     if (!token) {
-      navigate("/login");
+      navigate('/');
       return;
     }
 
@@ -39,17 +37,17 @@ export const CartProvider: React.FC<{ children: React.ReactNode }> = ({
         });
       }
     } catch (error: any) {
-      console.error("Error al obtener el carrito", error.message);
+      console.error('Error al obtener el carrito', error.message);
 
       if (error.response && error.response.status === 401) {
-        localStorage.removeItem("token");
-        navigate("/login");
+        localStorage.removeItem('token');
+        navigate('/');
       }
     }
   }, []);
 
   const addToCart = async (product: Product) => {
-    const token = localStorage.getItem("token");
+    const token = localStorage.getItem('token');
     if (!token) return;
 
     try {
@@ -61,35 +59,28 @@ export const CartProvider: React.FC<{ children: React.ReactNode }> = ({
         setCartState({ products, id_shipping });
       }
     } catch (error) {
-      console.error("Error al agregar al carrito", error);
+      console.error('Error al agregar al carrito', error);
     }
   };
 
-  const updateCart = async (
-    productInCart: ProductInCart,
-    id_shipping?: string
-  ) => {
-    const token = localStorage.getItem("token");
+  const updateCart = async (productInCart: ProductInCart, id_shipping?: string) => {
+    const token = localStorage.getItem('token');
     if (!token) return;
 
     try {
-      const response = await updateCartService(
-        token,
-        productInCart,
-        id_shipping
-      );
+      const response = await updateCartService(token, productInCart, id_shipping);
       if (response.status === 200) {
         const { products, id_shipping } = response.data;
 
         setCartState({ products, id_shipping });
       }
     } catch (error) {
-      console.error("Error al actualizar el carrito", error);
+      console.error('Error al actualizar el carrito', error);
     }
   };
 
   const removeFromCart = async (productId: string) => {
-    const token = localStorage.getItem("token");
+    const token = localStorage.getItem('token');
     if (!token) return;
 
     try {
@@ -100,7 +91,7 @@ export const CartProvider: React.FC<{ children: React.ReactNode }> = ({
         setCartState({ products, id_shipping });
       }
     } catch (error) {
-      console.error("Error al eliminar del carrito", error);
+      console.error('Error al eliminar del carrito', error);
     }
   };
 
@@ -127,7 +118,7 @@ export const CartProvider: React.FC<{ children: React.ReactNode }> = ({
 export const useCart = () => {
   const context = useContext(CartContext);
   if (!context) {
-    throw new Error("useCart must be used within a CartProvider");
+    throw new Error('useCart must be used within a CartProvider');
   }
   return context;
 };
